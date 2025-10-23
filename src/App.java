8efa75c110e27900e94446a243abcd3917e9de2a
EduardoLC17 <eduardo.lopezcozar@gmail.com>
2025-10-23 13:17:34 +0200

    v.0.2 saludo a la persona

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+
+import java.util.Scanner;
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Holaaaaa, soyyy Eduardoooo");
+        System.out.println("Holaaaaa, como te llamas?");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        System.out.println("Encantado" +  nombre);
     }
 }
